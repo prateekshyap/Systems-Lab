30807fddc5d901d8cb0b4dcbe6e8d98746a641ca
prateekshyap <prateekshyapriyadarshini@gmail.com>
2022-01-31 11:59:42 +0530

    Add Q1

--- Assignment1/Solutions/Q1/Restaurant.java
+++ Assignment1/Solutions/Q1/Restaurant.java
@@ -10,61 +10,47 @@ import java.util.Comparator;
 class Dish
 {
 	private int id; //unique order id
-	private int arrival; // arrival time
-	private int duration; // remaining time
-	Dish(int id, int arrival, int burst)
+	private int arrivalTime; // arrival time
+	private int remainingTime; // remaining time
+	Dish(int id, int arrivalTime, int burstTime)
 	{
 		this.id = id;
-		this.arrival = arrival;
-		this.duration = burst;
+		this.arrivalTime = arrivalTime;
+		this.remainingTime = burstTime;
 	}
 	public int getId() { return this.id; }
-	public int getArrival() { return this.arrival; }
-	public int getDuration() { return this.duration; }
+	public int getArrivalTime() { return this.arrivalTime; }
+	public int getRemainingTime() { return this.remainingTime; }
 	
-	public void setDuration (int d) { this.duration = d; }
+	public void setRemainingTime (int d) { this.remainingTime = d; }
 }
 
 class Heap
 {
-	private Dish[] allDishes; // queue for all the dishes
+	private Dish[] dishQueue; //queue for all the dishes
 	private Dish[] dishes; //heap array of dishes
-	private int heapSize;  
+	private int heapSize;
+	private int queueIndex;  
 	Heap(int[] a, int[] b)
 	{
-		this.allDishes = new Dish[a.length];
+		this.dishQueue = new Dish[a.length];
 		this.dishes = new Dish[a.length];
 		this.heapSize = 0;
-		int index = 0;
-		boolean addedToHeap = false;
-		int j = 0;
-		for (int i = 0; i < a.length; ++i)//TODO: confusing logic
-		{
-			addedToHeap = false;
-			if (this.dishes[0] != null && this.dishes[0].getArrival() > a[i])
-			{
-				for (j = 0; j < heapSize; ++j) this.allDishes[index++] = this.dishes[j];
-				this.dishes = new Dish[a.length];
-				this.heapSize = 0;
-			}
-			if (this.dishes[0] == null || this.dishes[0].getArrival() == a[i])
-			{
-				this.dishes[heapSize++] = new Dish(i,a[i],b[i]);
-				addedToHeap = true;
-			}
-			if (!addedToHeap) this.allDishes[index++] = new Dish(i,a[i],b[i]);
-		}
-		Dish[] temp = new Dish[index];
-		for (int i = 0; i < index; ++i)
-			temp[i] = allDishes[i];
-		allDishes = temp;
-		build();
-		Arrays.sort(allDishes,new Comparator<Dish>(){
+		this.queueIndex = a.length-1;
+		//int index = 0;
+		int i = 0;
+		for (i = 0; i < a.length; ++i)
+			dishQueue[i] = new Dish(i,a[i],b[i]);
+		Arrays.sort(dishQueue,new Comparator<Dish>(){
 			public int compare(Dish dish1, Dish dish2)
 			{
-				return dish1.getArrival()-dish2.getArrival();
+				return dish2.getArrivalTime()-dish1.getArrivalTime();
 			}
 		});
+		int minArrivalTime = dishQueue[queueIndex].getArrivalTime();
+		while (queueIndex >= 0 && dishQueue[queueIndex].getArrivalTime() == minArrivalTime)
+			dishes[heapSize++] = dishQueue[queueIndex--];
+		build();
 		//printHeap();
 	}
 	private int left(int i) { return (2*i)+1; }
@@ -80,20 +66,20 @@ class Heap
 		int min = root;
 		if (left(root) < heapSize)
 		{
-			if (dishes[left(root)].getDuration() < dishes[min].getDuration())
+			if (dishes[left(root)].getRemainingTime() < dishes[min].getRemainingTime())
 				min = left(root);
-			else if (dishes[left(root)].getDuration() == dishes[min].getDuration() && dishes[left(root)].getArrival() < dishes[min].getArrival())
+			else if (dishes[left(root)].getRemainingTime() == dishes[min].getRemainingTime() && dishes[left(root)].getArrivalTime() < dishes[min].getArrivalTime())
 				min = left(root);
-			else if (dishes[left(root)].getDuration() == dishes[min].getDuration() && dishes[left(root)].getArrival() == dishes[min].getArrival() && dishes[left(root)].getId() < dishes[min].getId())
+			else if (dishes[left(root)].getRemainingTime() == dishes[min].getRemainingTime() && dishes[left(root)].getArrivalTime() == dishes[min].getArrivalTime() && dishes[left(root)].getId() < dishes[min].getId())
 				min = left(root);
 		}
 		if (right(root) < heapSize)
 		{
-			if (dishes[right(root)].getDuration() < dishes[min].getDuration())
+			if (dishes[right(root)].getRemainingTime() < dishes[min].getRemainingTime())
 				min = right(root);
-			else if (dishes[right(root)].getDuration() == dishes[min].getDuration() && dishes[right(root)].getArrival() < dishes[min].getArrival())
+			else if (dishes[right(root)].getRemainingTime() == dishes[min].getRemainingTime() && dishes[right(root)].getArrivalTime() < dishes[min].getArrivalTime())
 				min = right(root);
-			else if (dishes[right(root)].getDuration() == dishes[min].getDuration() && dishes[right(root)].getArrival() == dishes[min].getArrival() && dishes[right(root)].getId() < dishes[min].getId())
+			else if (dishes[right(root)].getRemainingTime() == dishes[min].getRemainingTime() && dishes[right(root)].getArrivalTime() == dishes[min].getArrivalTime() && dishes[right(root)].getId() < dishes[min].getId())
 				min = right(root);
 		}
 		if (min != root)
@@ -119,8 +105,8 @@ class Heap
 	}
 	public Dish seeNextArrivalPoint()
 	{
-		if (allDishes == null || allDishes.length == 0) return null;
-		return allDishes[0];
+		if (dishQueue == null || queueIndex < 0) return null;
+		return dishQueue[queueIndex];
 	}
 	public void putBack(Dish dish) //put back the dish with remaining time into heap
 	{
@@ -128,7 +114,7 @@ class Heap
 		int i = heapSize;
 		++heapSize;
 		Dish temp = null;
-		while (parent(i) >= 0 && ((dishes[parent(i)].getDuration() > dishes[i].getDuration()) || (dishes[parent(i)].getDuration() == dishes[i].getDuration() && dishes[parent(i)].getArrival() > dishes[i].getArrival()) || (dishes[parent(i)].getDuration() == dishes[i].getDuration() && dishes[parent(i)].getArrival() == dishes[i].getArrival() && dishes[parent(i)].getId() > dishes[i].getId())))
+		while (parent(i) >= 0 && ((dishes[parent(i)].getRemainingTime() > dishes[i].getRemainingTime()) || (dishes[parent(i)].getRemainingTime() == dishes[i].getRemainingTime() && dishes[parent(i)].getArrivalTime() > dishes[i].getArrivalTime()) || (dishes[parent(i)].getRemainingTime() == dishes[i].getRemainingTime() && dishes[parent(i)].getArrivalTime() == dishes[i].getArrivalTime() && dishes[parent(i)].getId() > dishes[i].getId())))
 		{
 			temp = dishes[i];
 			dishes[i] = dishes[parent(i)];
@@ -143,36 +129,26 @@ class Heap
 	}
 	public void updateHeap(int timePoint)
 	{
-		int i = 0;
-		for (i = 0; i < allDishes.length; ++i)
+		while (queueIndex >= 0 && dishQueue[queueIndex].getArrivalTime() == timePoint)
+			putBack(dishQueue[queueIndex--]);
+		if (queueIndex == -1)
 		{
-			if (allDishes[i].getArrival() == timePoint)
-				putBack(allDishes[i]);
-			else break;
-		}
-		if (i == allDishes.length)
-		{
-			allDishes = null;
+			dishQueue = null;
 			return;
 		}
-		Dish[] temp = new Dish[allDishes.length-i]; //TODO: decrease order
-		int j = allDishes.length-1;
-		for (i = temp.length-1; i >= 0; --i)
-			temp[i] = allDishes[j--];
-		allDishes = temp;
 	}
-	/*public void printHeap()
+	public void printHeap()
 	{
 		System.out.println("Heap-");
 		for (int i = 0; i < heapSize; ++i)
-			System.out.print("("+dishes[i].getId()+","+dishes[i].getArrival()+","+dishes[i].getDuration()+")-");
+			System.out.print("("+dishes[i].getId()+","+dishes[i].getArrivalTime()+","+dishes[i].getRemainingTime()+")-");
 		System.out.println();
 		System.out.println("Queue-");
-		if (allDishes != null)
-			for (int i = 0; i < allDishes.length; ++i)
-				System.out.print("("+allDishes[i].getId()+","+allDishes[i].getArrival()+","+allDishes[i].getDuration()+")-");
+		if (dishQueue != null)
+			for (int i = 0; i <= queueIndex; ++i)
+				System.out.print("("+dishQueue[i].getId()+","+dishQueue[i].getArrivalTime()+","+dishQueue[i].getRemainingTime()+")-");
 		System.out.println();
-	}*/
+	}
 }
 
 class Restaurant
@@ -183,8 +159,9 @@ class Restaurant
 		String line = "";
 		String[] tokens = null;
 		int n = 10; // CAN CHANGE // number of process
-		int i = 0, timer = 0, currDuration = -1;
-		int[] arrivalTimestamps = new int[n], burstTimes = new int[n];
+		int i = 0, j = 0, timer = 0, currDuration = -1;
+		double avgWaitingTime = 0.0;
+		int[] arrivalTimestamps = new int[n], burstTimes = new int[n], completionTimestamps = new int[n], turnAroundTimestamps = new int[n], waitingTimestamps = new int[n];
 		ArrayList<Integer> startTimes = null;
 		ArrayList<Integer> stopTimes = null;
 		ArrayList<Integer> ids = null;
@@ -209,7 +186,7 @@ class Restaurant
 			}
 			heap = new Heap(arrivalTimestamps,burstTimes);
 
-			timer = heap.seeNextDish().getArrival();
+			timer = heap.seeNextDish().getArrivalTime();
 			if (timer != 0)
 			{
 				startTimes.add(0);
@@ -222,9 +199,9 @@ class Restaurant
 				currDish = heap.getNextDish();
 				nextDish = heap.seeNextArrivalPoint();
 				if (nextDish != null)
-					currDuration = Math.min(currDish.getDuration(),nextDish.getArrival()-timer);
+					currDuration = Math.min(currDish.getRemainingTime(),nextDish.getArrivalTime()-timer);
 				else
-					currDuration = currDish.getDuration();
+					currDuration = currDish.getRemainingTime();
 				
 				if (ids.size() != 0 && (Integer)ids.get(ids.size()-1) == currDish.getId())
 					stopTimes.set(stopTimes.size()-1,new Integer(timer+currDuration));
@@ -234,50 +211,103 @@ class Restaurant
 					stopTimes.add(timer+currDuration);
 					ids.add(currDish.getId());
 				}
-				currDish.setDuration(currDish.getDuration()-currDuration);
-				if (currDish.getDuration() > 0)
+				currDish.setRemainingTime(currDish.getRemainingTime()-currDuration);
+				if (currDish.getRemainingTime() > 0)
 					heap.putBack(currDish);
 
 				timer += currDuration;
-				if (nextDish != null && nextDish.getArrival() > timer)
+				if (nextDish != null && nextDish.getArrivalTime() > timer)
 				{
 					if (!heap.isEmpty())
 						continue;
 					if (ids.size() != 0 && (Integer)ids.get(ids.size()-1) == -1)
-						stopTimes.set(stopTimes.size()-1,new Integer(nextDish.getArrival()));
+						stopTimes.set(stopTimes.size()-1,new Integer(nextDish.getArrivalTime()));
 					else
 					{
 						startTimes.add(timer);
-						stopTimes.add(nextDish.getArrival());
+						stopTimes.add(nextDish.getArrivalTime());
 						ids.add(-1);
 					}
 				}
-				else if (nextDish != null && nextDish.getArrival() == timer)
-				{
+				else if (nextDish != null && nextDish.getArrivalTime() == timer)
 					heap.updateHeap(timer);
-				}
 				else if (nextDish != null)
-				{
 					System.out.println("Calculation went wrong");
-				}
 			}
 
+			//calculating timestamps
+			for (i = 0; i < ids.size(); ++i) //completion time
+				if ((Integer)ids.get(i) != -1) completionTimestamps[(Integer)ids.get(i)] = (Integer)stopTimes.get(i);
+			for (i = 0; i < n; ++i) //turn around time
+				turnAroundTimestamps[i] = completionTimestamps[i] - arrivalTimestamps[i];
+			for (i = 0; i < n; ++i) //waiting time
+				waitingTimestamps[i] = turnAroundTimestamps[i] - burstTimes[i];
+
+
+			//printing process details and gantt chart
 			System.out.println();
-			System.out.println("Given Testcase-");
-			System.out.print("AT- ");
-			for (i = 0; i < arrivalTimestamps.length; ++i)
-				System.out.print(arrivalTimestamps[i]+" ");
 			System.out.println();
-			System.out.print("BT- ");
-			for (i = 0; i < arrivalTimestamps.length; ++i)
-				System.out.print(burstTimes[i]+" ");
+			System.out.println("===============================================================================================================");
 			System.out.println();
-			System.out.println("START\tEND\tID");
+			System.out.println("Process Details-");
+			System.out.println("---------------------------------------------");
+			System.out.println("|  PID  |  AT  |  BT  |  CT  |  TAT  |  WT  |");
+			System.out.println("---------------------------------------------");
+			for (i = 0; i < n; ++i)
+				System.out.println("|   "+i+"   |  "+(arrivalTimestamps[i] < 10 ? " " : "")+arrivalTimestamps[i]+"  |   "+burstTimes[i]+"  |  "+(completionTimestamps[i] < 10 ? " " : "")+completionTimestamps[i]+"  |   "+(turnAroundTimestamps[i] < 10 ? " " : "")+turnAroundTimestamps[i]+"  |  "+(waitingTimestamps[i] < 10 ? " " : "")+waitingTimestamps[i]+"  |");
+			System.out.println("---------------------------------------------");
+			System.out.println();
+			System.out.println("Gantt Chart-");
+			System.out.print("-");
 			for (i = 0; i < ids.size(); ++i)
-				System.out.println(startTimes.get(i)+"\t"+stopTimes.get(i)+"\t"+((Integer)ids.get(i) == -1 ? "idle" : ids.get(i)));
+			{
+				currDuration = (Integer)stopTimes.get(i)-(Integer)startTimes.get(i);
+				for (j = 0; j < currDuration; ++j)
+					System.out.print("--");
+				System.out.print("-");
+			}
+			System.out.println();
+			System.out.print("|");
+			for (i = 0; i < ids.size(); ++i)
+			{
+				currDuration = (Integer)stopTimes.get(i)-(Integer)startTimes.get(i);
+				for (j = 0; j < (currDuration/2)-1; ++j)
+					System.out.print("  ");
+				if ((Integer)ids.get(i) == -1) System.out.print("NA");
+				else if ((Integer)ids.get(i) < 10) System.out.print(" "+(Integer)ids.get(i));
+				else System.out.print((Integer)ids.get(i));
+				for (; j < currDuration-1; ++j)
+					System.out.print("  ");
+				System.out.print("|");
+			}
+			System.out.println();
+			System.out.print("-");
+			for (i = 0; i < ids.size(); ++i)
+			{
+				currDuration = (Integer)stopTimes.get(i)-(Integer)startTimes.get(i);
+				for (j = 0; j < currDuration; ++j)
+					System.out.print("--");
+				System.out.print("-");
+			}
+			System.out.println();
+			System.out.print("0");
+			for (i = 0; i < ids.size(); ++i)
+			{
+				currDuration = (Integer)stopTimes.get(i)-(Integer)startTimes.get(i);
+				for (j = 0; j < currDuration-1; ++j)
+					System.out.print("  ");
+				System.out.print(((Integer)stopTimes.get(i) < 10 ? "  " : " ")+(Integer)stopTimes.get(i));
+			}
+			System.out.println();
+			System.out.println();
+			System.out.println("Average Waiting Time-");
+			for (i = 0; i < n; ++i)
+				avgWaitingTime += (double)waitingTimestamps[i];
+			avgWaitingTime /= n;
+			System.out.println(avgWaitingTime);
+			System.out.println();
 		}
 
 		reader.close();
 	}
 }
-//TODO: gantt chart printing
